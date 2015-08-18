909834db9c4d7850dc5ff1c971062230e420d759
Nate <palmern22@gmail.com>
2015-08-17 22:45:59 -0400

    Add nunit nuget, change tests output add test

---
+++ src/CattleTrack/CattleTrack.Tests/CattleTrackTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CattleTrack.Tests
+{
+    [TestFixture]
+    public class CattleTrackTests
+    {
+        [Test]
+        public void TestNUnitWorks()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
