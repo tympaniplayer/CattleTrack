b713b74ecb10123780ee0150b0e188ddb529f671
Nate <palmern22@gmail.com>
2015-12-21 22:23:22 -0500

    Add solution for server, add nancy web project with hello world module

---
+++ server/CattleTrack/CattleTrack.Web/HelloModule.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CattleTrack.Web
+{
+    public class HelloModule : NancyModule
+    {
+        public HelloModule()
+        {
+            Get["/"] = parameters => "Hello World";
+            
+    }
+}
