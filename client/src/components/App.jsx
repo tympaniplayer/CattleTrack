75576a0a3c7810d1282d9517bec6a989bfe17ec5
Nate <palmern22@gmail.com>
2015-12-29 06:44:40 -0500

    Start creating left nav

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,6 +3,8 @@ require('../sass/main.scss');
 import React from 'react';
 import { render } from 'react-dom';
 import AppBar from 'material-ui/lib/app-bar';
+import LeftNav from 'material-ui/lib/left-nav';
+import MenuItem from 'material-ui/lib/menus/menu-item';
 import About from './About.jsx';
 
 import Theme from './theme/theme.js';
@@ -16,16 +18,18 @@ class App extends React.Component {
     super();
   };
 
+  handleToggle = () => this.setState({open: !this.state.open});
+
   render(){
     return(
       <div>
-        <AppBar title="Cattle Track" style={{
-          marginBottom: "10px"
-        }}/>
-        <ul>
-          <li><Link to="/about">About</Link></li>
-
-        </ul>
+        <AppBar title="Cattle Track"
+          style={{
+            marginBottom: "10px"
+          }}
+          onLeftIconButtonTouchTap={this.handleToggle}
+        />
+        <LeftNav
         {this.props.children}
       </div>
     );
